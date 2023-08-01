2a72d13d977534066e035a2222f4341395a9884f
SoftwareBound <benzvi.ron@gmail.com>
2023-08-01 11:24:43 +0300

    Support sending all data to client

--- mongoDB.ts
+++ mongoDB.ts
@@ -1,28 +1,48 @@
 import { MongoClient } from "mongodb";
 
 const client = new MongoClient("mongodb://localhost:27017/Recipes");
+let dbConnection: MongoClient;
 
 export async function insertOne(dataToInsert: Object) {
-  let data = await dbConnect();
-  data.insertOne(dataToInsert);
+  try {
+    let data = await dbConnect();
+    await data.insertOne(dataToInsert);
+  } catch {
+    throw Error("couldnt perform insertOne action ");
+  } finally {
+    dbConnection.close();
+  }
 }
 
 export async function findOne(dataToFind: Object) {
   try {
     let data = await dbConnect();
     const result = await data.findOne(dataToFind);
-    const JSONResult = JSON.parse(JSON.stringify(result));
-    return JSONResult;
+    return JSON.parse(JSON.stringify(result));
   } catch {
     throw Error("couldnt perform findOne action");
+  } finally {
+    dbConnection.close();
+  }
+}
+
+export async function find() {
+  try {
+    let data = await dbConnect();
+    const result = await data.find().toArray();
+    return JSON.parse(JSON.stringify(result));
+  } catch {
+    throw Error("couldnt perform find action");
+  } finally {
+    dbConnection.close();
   }
 }
 
 async function dbConnect() {
   try {
-    const connection = await client.connect();
+    dbConnection = await client.connect();
     console.log("connected to db successful");
-    const db = connection.db("Recipes");
+    const db = dbConnection.db("Recipes");
     return db.collection("recipes");
   } catch {
     throw Error("connection to db failed");
